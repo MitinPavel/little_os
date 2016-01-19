6260a3199f18afdf8912827758f954630ac357fd
pm <mitin.pavel@gmail.com>
2016-01-19 17:09:00 +0000

    Set up a stack.

--- loader.s
+++ loader.s
@@ -2,11 +2,18 @@ extern cafebabe
 
 global loader                   ; the entry symbol for ELF
 
+KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+
 MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
 FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
 
+
+section .bss
+align 4                         ; align at 4 bytes
+kernel_stack:                   ; label points to beginning of memory
+    resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
 
 section .text:                  ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
@@ -14,6 +21,8 @@ align 4                         ; the code must be 4 byte aligned
     dd FLAGS                    ; the flags,
     dd CHECKSUM                 ; and the checksum
 
+mov esp, kernel_stack + KERNEL_STACK_SIZE       ; point esp to the start of the
+                                                ; stack (end of memory area)
 loader:                         ; the loader label (defined as entry point in linker script)
     call cafebabe 
 
