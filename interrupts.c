ec6e758eb85852c3e629e9760d02c67f5a26683b
pm <mitin.pavel@gmail.com>
2016-02-19 17:38:27 +0000

    Do refactoring: add INTERRUPTS_ suffix to constants.

--- interrupts.c
+++ interrupts.c
@@ -62,17 +62,17 @@ void interrupts_install_idt()
 	rather than 8h and 70h, as configured by default.
 */
 
-#define ICW1_ICW4	0x01		/* ICW4 (not) needed */
-#define ICW1_SINGLE	0x02		/* Single (cascade) mode */
-#define ICW1_INTERVAL4	0x04		/* Call address interval 4 (8) */
-#define ICW1_LEVEL	0x08		/* Level triggered (edge) mode */
-#define ICW1_INIT	0x10		/* Initialization - required! */
+#define INTERRUPTS_ICW1_ICW4		0x01		/* ICW4 (not) needed */
+#define INTERRUPTS_ICW1_SINGLE		0x02		/* Single (cascade) mode */
+#define INTERRUPTS_ICW1_INTERVAL4	0x04		/* Call address interval 4 (8) */
+#define INTERRUPTS_ICW1_LEVEL		0x08		/* Level triggered (edge) mode */
+#define INTERRUPTS_ICW1_INIT		0x10		/* Initialization - required! */
 
-#define ICW4_8086	0x01		/* 8086/88 (MCS-80/85) mode */
-#define ICW4_AUTO	0x02		/* Auto (normal) EOI */
-#define ICW4_BUF_SLAVE	0x08		/* Buffered mode/slave */
-#define ICW4_BUF_MASTER	0x0C		/* Buffered mode/master */
-#define ICW4_SFNM	0x10		/* Special fully nested (not) */
+#define INTERRUPTS_ICW4_8086		0x01		/* 8086/88 (MCS-80/85) mode */
+#define INTERRUPTS_ICW4_AUTO		0x02		/* Auto (normal) EOI */
+#define INTERRUPTS_ICW4_BUF_SLAVE	0x08		/* Buffered mode/slave */
+#define INTERRUPTS_ICW4_BUF_MASTER	0x0C		/* Buffered mode/master */
+#define INTERRUPTS_ICW4_SFNM		0x10		/* Special fully nested (not) */
 
 /** pic_acknowledge:
      *  Acknowledges an interrupt from either PIC 1 or PIC 2.
@@ -101,15 +101,15 @@ arguments:
 
 void interrupts_remap_pic(int offset1, int offset2)
 {
-	outb(PIC1_COMMAND, ICW1_INIT+ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
-	outb(PIC2_COMMAND, ICW1_INIT+ICW1_ICW4);
+	outb(PIC1_COMMAND, INTERRUPTS_ICW1_INIT+INTERRUPTS_ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
+	outb(PIC2_COMMAND, INTERRUPTS_ICW1_INIT+INTERRUPTS_ICW1_ICW4);
 	outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
 	outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
 	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
 	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
 
-	outb(PIC1_DATA, ICW4_8086);
-	outb(PIC2_DATA, ICW4_8086);
+	outb(PIC1_DATA, INTERRUPTS_ICW4_8086);
+	outb(PIC2_DATA, INTERRUPTS_ICW4_8086);
 
         // Setup Interrupt Mask Register (IMR)
 	outb(PIC1_DATA, 0xFD); // 1111 1101 - Enable IRQ 1 only (keyboard).
