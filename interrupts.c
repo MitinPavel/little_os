97c5109c9615af851bf9f7c852e954d98a565fe7
pm <mitin.pavel@gmail.com>
2016-02-19 18:07:45 +0000

    Extract PIC related code to a separate files.

--- interrupts.c
+++ interrupts.c
@@ -1,19 +1,11 @@
 #include "interrupts.h"
+#include "pic.h"
 #include "io.h"
 
 #include "serial.h"
 #include "keyboard.h"
 
 #define INTERRUPTS_DESCRIPTOR_COUNT 256 
-
-#define INTERRUPTS_PIC1_OFFSET 0x20
-#define INTERRUPTS_PIC2_OFFSET 0x28
-#define INTERRUPTS_PIC2_END INTERRUPTS_PIC2_OFFSET + 7
-
-#define INTERRUPTS_PIC1_COMMAND_PORT 0x20
-#define INTERRUPTS_PIC2_COMMAND_PORT 0xA0
-#define INTERRUPTS_PIC_ACKNOWLEDGE 0x20
-
 #define INTERRUPTS_KEYBOARD 33 
 
 struct IDTDescriptor idt_descriptors[INTERRUPTS_DESCRIPTOR_COUNT];
@@ -50,73 +42,9 @@ void interrupts_install_idt()
 	idt.size = sizeof(struct IDTDescriptor) * INTERRUPTS_DESCRIPTOR_COUNT;
 	interrupts_load_idt((int) &idt);
 
-	interrupts_remap_pic(INTERRUPTS_PIC1_OFFSET, INTERRUPTS_PIC2_OFFSET);
+	pic_remap(PIC_PIC1_OFFSET, PIC_PIC2_OFFSET);
 }
 
-/* PIC ***********************************************************************/
-
-/*
-	From: http://wiki.osdev.org/PIC
-
-	Reinitialize the PIC controllers, giving them specified vector offsets
-	rather than 8h and 70h, as configured by default.
-*/
-
-#define INTERRUPTS_ICW1_ICW4		0x01		/* ICW4 (not) needed */
-#define INTERRUPTS_ICW1_SINGLE		0x02		/* Single (cascade) mode */
-#define INTERRUPTS_ICW1_INTERVAL4	0x04		/* Call address interval 4 (8) */
-#define INTERRUPTS_ICW1_LEVEL		0x08		/* Level triggered (edge) mode */
-#define INTERRUPTS_ICW1_INIT		0x10		/* Initialization - required! */
-
-#define INTERRUPTS_ICW4_8086		0x01		/* 8086/88 (MCS-80/85) mode */
-#define INTERRUPTS_ICW4_AUTO		0x02		/* Auto (normal) EOI */
-#define INTERRUPTS_ICW4_BUF_SLAVE	0x08		/* Buffered mode/slave */
-#define INTERRUPTS_ICW4_BUF_MASTER	0x0C		/* Buffered mode/master */
-#define INTERRUPTS_ICW4_SFNM		0x10		/* Special fully nested (not) */
-
-/** pic_acknowledge:
-     *  Acknowledges an interrupt from either PIC 1 or PIC 2.
-     *
-     *  @param num The number of the interrupt
-     */
-void interrupts_pic_acknowledge(unsigned int interrupt)
-{
-	if (interrupt < INTERRUPTS_PIC1_OFFSET || interrupt > INTERRUPTS_PIC2_END) {
-		return;
-	}
-
-	if (interrupt < INTERRUPTS_PIC2_OFFSET) {
-		outb(INTERRUPTS_PIC1_COMMAND_PORT, INTERRUPTS_PIC_ACKNOWLEDGE);
-	} else {
-		outb(INTERRUPTS_PIC2_COMMAND_PORT, INTERRUPTS_PIC_ACKNOWLEDGE);
-	}
-}
-
-/*
-arguments:
-	offset1 - vector offset for master PIC
-		vectors on the master become offset1..offset1+7
-	offset2 - same for slave PIC: offset2..offset2+7
-*/
-
-void interrupts_remap_pic(int offset1, int offset2)
-{
-	outb(PIC1_COMMAND, INTERRUPTS_ICW1_INIT+INTERRUPTS_ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
-	outb(PIC2_COMMAND, INTERRUPTS_ICW1_INIT+INTERRUPTS_ICW1_ICW4);
-	outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
-	outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
-	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
-	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
-
-	outb(PIC1_DATA, INTERRUPTS_ICW4_8086);
-	outb(PIC2_DATA, INTERRUPTS_ICW4_8086);
-
-        // Setup Interrupt Mask Register (IMR)
-	outb(PIC1_DATA, 0xFD); // 1111 1101 - Enable IRQ 1 only (keyboard).
-	outb(PIC2_DATA, 0xFF);
-
-	asm("sti"); // Enable interrupts.
-}
 
 /* Interrupt handlers ********************************************************/
 
@@ -139,7 +67,7 @@ void interrupt_handler(__attribute__((unused)) struct cpu_state cpu, unsigned in
 				serial_write(str, 1);
 			}
 
-			interrupts_pic_acknowledge(interrupt);
+			pic_acknowledge(interrupt);
 
 			break;
 		default:
--- interrupts.h
+++ interrupts.h
@@ -1,14 +1,6 @@
 #ifndef INCLUDE_INTERRUPTS
 #define INCLUDE_INTERRUPTS
 
-/*                      I/O port */
-#define PIC1		0x20		/* IO base address for master PIC */
-#define PIC2		0xA0		/* IO base address for slave PIC */
-#define PIC1_COMMAND	PIC1
-#define PIC1_DATA	(PIC1+1)
-#define PIC2_COMMAND	PIC2
-#define PIC2_DATA	(PIC2+1)
-
 struct IDT 
 {
 	unsigned short size;
@@ -27,8 +19,6 @@ struct IDTDescriptor {
 } __attribute__((packed));
 
 void interrupts_install_idt();
-
-void interrupts_remap_pic(int offset1, int offset2);
 
 // Wrappers around ASM.
 void interrupts_load_idt(unsigned int idt_address);
